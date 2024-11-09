49a18524a6aa9cedd7da891d3b6d0178b1273425
Pranav_Stark <pranavstark21@gmail.com>
2024-11-09 07:23:49 +0530

    Update app.component.ts

--- Angular_Template_Forms/src/app/app.component.ts
+++ Angular_Template_Forms/src/app/app.component.ts
@@ -6,20 +6,11 @@ import { TooltipModule } from 'ngx-bootstrap/tooltip';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,TooltipModule,RouterLink,RouterLinkActive],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'Angular_Template_Forms';
-  constructor(private router:Router){}
 
-  GotoUserList(){
-    this.router.navigate(['/user-list']);
-  }
-
-  GotoTaskList(){
-    this.router.navigate(['/task-list']);
-  }
   
 }
